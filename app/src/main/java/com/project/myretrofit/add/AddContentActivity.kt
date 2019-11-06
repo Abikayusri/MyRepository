221d9ae6f0dc9799f22e8a23eadbfae1f9045861
Abikayusri <abikayusri96@gmail.com>
2019-11-06 22:02:07 +0700

    add method presenter

--- app/src/main/java/com/project/myretrofit/add/AddContentActivity.kt
+++ app/src/main/java/com/project/myretrofit/add/AddContentActivity.kt
@@ -11,16 +11,20 @@ import kotlinx.android.synthetic.main.activity_add_content.*
 import retrofit2.Callback
 import retrofit2.Response
 
-class AddContentActivity : AppCompatActivity() {
+class AddContentActivity : AppCompatActivity(), AddContentPresenter.Listener {
 
-//    private lateinit var addContentPresenter: AddContentPresenter
+    private lateinit var addContentPresenter: AddContentPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_content)
 
-
+        setupComponent()
         setupListener()
+    }
+
+    private fun setupComponent() {
+        addContentPresenter = AddContentPresenter(this)
     }
 
     private fun setupListener() {
@@ -45,53 +49,35 @@ class AddContentActivity : AppCompatActivity() {
                 }
             }
 
-//            if (title.isEmpty()) {
-//                et_add_title.error = "Title required"
-//                et_add_title.requestFocus()
-//                return@setOnClickListener
-//            }
-//
-//            if (categories.isEmpty()) {
-//                et_add_categories.error = "Categories required"
-//                et_add_categories.requestFocus()
-//                return@setOnClickListener
-//            }
-//
-//            if (content.isEmpty()) {
-//                et_add_content.error = "Content required"
-//                et_add_content.requestFocus()
-//                return@setOnClickListener
-//            }
-
-
             val postContent = PostContentResponse(title, categories, content)
-//            addContentPresenter.createContent(postContent)
+            addContentPresenter.createContent(postContent)
 
-
-            ApiClient.instance.addContent(postContent)
-                .enqueue(object : Callback<DefaultResponse> {
-                    override fun onFailure(call: retrofit2.Call<DefaultResponse>, t: Throwable) {
-                        Toast.makeText(this@AddContentActivity, "Error", Toast.LENGTH_SHORT).show()
-                    }
-
-                    override fun onResponse(
-                        call: retrofit2.Call<DefaultResponse>, response: Response<DefaultResponse>
-                    ) {
-                        Toast.makeText(this@AddContentActivity, "Sukses", Toast.LENGTH_SHORT).show()
-                    }
-
-                })
+//            ApiClient.instance.addContent(postContent)
+//                .enqueue(object : Callback<DefaultResponse> {
+//                    override fun onFailure(call: retrofit2.Call<DefaultResponse>, t: Throwable) {
+//                        Toast.makeText(this@AddContentActivity, "Error", Toast.LENGTH_SHORT).show()
+//                    }
+//
+//                    override fun onResponse(
+//                        call: retrofit2.Call<DefaultResponse>, response: Response<DefaultResponse>
+//                    ) {
+//                        Toast.makeText(this@AddContentActivity, "Sukses", Toast.LENGTH_SHORT).show()
+//                    }
+//
+//                })
         }
     }
 
-//    override fun onAddContentSuccess(sccMessage: String) {
-//        Toast.makeText(this, sccMessage, Toast.LENGTH_SHORT).show()
-//        finish()
-//    }
-//
-//    override fun onAddContentFailure(errMessage: String) {
-//        Toast.makeText(this, errMessage, Toast.LENGTH_SHORT).show()
-//    }
+
+
+    override fun onAddContentSuccess(sccMessage: String) {
+        Toast.makeText(this, sccMessage, Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    override fun onAddContentFailure(errMessage: String) {
+        Toast.makeText(this, errMessage, Toast.LENGTH_SHORT).show()
+    }
 
 //    override fun onAddContentLoading(isLoading: Boolean) {
 //    }
